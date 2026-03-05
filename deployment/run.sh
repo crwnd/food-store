9334c2b7ccaa5f96653435a916893dcace62b3a9
Alex Trukhin <141617261+alextrukhin@users.noreply.github.com>
2026-03-05 12:22:36 +0100

    Update run.sh

--- deployment/run.sh
+++ deployment/run.sh
@@ -2,4 +2,4 @@
 
 # cd /app/packages/prisma && npm i dotenv && npx prisma migrate deploy && rm -rf node_modules package-lock.json
 # cd /app && node --import ./packages/isofarmresearch/.output/server/sentry.server.config.mjs packages/isofarmresearch/.output/server/index.mjs
-cd /app && node .output/server/index.mjs
+cd /app/packages/website && node .output/server/index.mjs
